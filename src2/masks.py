75bcb3e5e561737a33222396d8394768ad7e6e88
Ruslan Djabrailov <pasna05@mail.ru>
2026-03-09 11:48:20 +0300

    init

--- src2/masks.py
+++ src2/masks.py
@@ -1,14 +1,33 @@
-def get_mask_card_number(cart_num: str) -> str:
+def get_mask_card_number(card_num: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску."""
-    cart_str = str(cart_num)
-    if len(cart_str) != 16:
-        mask = f"{cart_str[:4]} {cart_str[4:7]}** **** {cart_str[-4:]}"
+    card_str = str(card_num)
+    if len(card_str) != 16:
+        raise ValueError("1234567812345678")
+        mask = f"{card_str[:4]} {card_str[4:6]}**{card_str[-4:]}"
+
+        return mask
+        return "Некоректный номер карты"
+    mask = f"{card_str[:4]} {card_str[4:6]}**{card_str[-4:]}"
     return mask
 
 
-def get_mask_account(banc_num: str) -> str:
+def get_mask_account(acc_num: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску."""
-    banc_str = str(banc_num)
-    if len(banc_str) != 6:
-        masked = f"**{banc_str[-4:]}"
-    return masked
+    acc_str = str(acc_num)
+    if len(acc_str) < 4:
+        return "Некоректный номер счета"
+
+    return f"**{acc_str[-4:]}"
+
+
+if __name__ == "__main__":
+    try:
+        card = "7000792289606361"
+        print(f"Карт: {get_mask_card_number(card)}")
+
+        account = "73654108430135874305"
+        print(f"Счет: {get_mask_account(account)}")
+
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
